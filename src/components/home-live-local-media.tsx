27f6e92293f0be373529a015ef5ff4157a29e021
Carter Steinhoff <cartersteinhoff@gmail.com>
2026-06-18 10:10:36 -0700

    Tighten live media artwork layout

--- src/components/home-live-local-media.tsx
+++ src/components/home-live-local-media.tsx
@@ -394,7 +394,7 @@ export function HomeLiveLocalMedia() {
               </div>
             </div>
 
-            <FeaturedMediaArtwork className="order-1 lg:order-2 lg:mx-auto lg:max-w-[84%]" />
+            <FeaturedMediaArtwork className="order-1 lg:order-2 lg:mx-0 lg:max-w-[80%]" />
           </div>
         </div>
       </div>
